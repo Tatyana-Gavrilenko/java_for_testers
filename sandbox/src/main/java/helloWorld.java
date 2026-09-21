31926fe1ac5ebe12392c5f535f06ea6c3e90abc2
tgavrilenko <gavrilenkota621@gmail.com>
2026-09-21 22:59:34 +0700

    Эксперименты с функциями

--- sandbox/src/main/java/helloWorld.java
+++ sandbox/src/main/java/helloWorld.java
@@ -1,5 +1,15 @@
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("hello, World!");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 3);
+        System.out.println(2 - 1);
+        System.out.println(2 / 3);
+
+        System.out.println("hello, " + "QA-" + "engineer");
+        System.out.println("2 + 3 = " + 5);
+        System.out.println("2 + 2 = " + 2 + 2);
+        System.out.println("2 + 2 = " + (2 + 2));
     }
 }
